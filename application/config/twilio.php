ae880f5d423350a9df7af00739d1420ee9048032
Sean Teague <voodoomon@gmail.com>
2015-04-12 14:02:35 -0700

    no longer tracking Twilio API config file

--- application/config/twilio.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-return array
-(
-	'AccountSid' => 'ACc1ffe8def1bfdb312938c33772b9dbc4',
-	'AuthToken' => 'e40bf7b91d8d52ddc71d4633744f3b68',
-	'AppNumber' => '(650) 560-7156',
-	'ApiUrl' => 'https://api.twilio.com/2010-04-01',
-);
